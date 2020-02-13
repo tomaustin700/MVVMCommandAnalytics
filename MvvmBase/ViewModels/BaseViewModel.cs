71e7cc916baf1eacd2ba98b83b655ca8eb5fead6
Tom Austin <Tom.Austin@streets-heaver.com>
2020-02-13 10:53:22 +0000

    Added RaiseCanExecuteChangedOnCommands

--- MvvmBase/ViewModels/BaseViewModel.cs
+++ MvvmBase/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MvvmBase.Classes;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,17 +7,41 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MvvmBase.ViewModels
 {
     public abstract class BaseViewModel : BaseNotify
     {
-       
+        List<DelegateCommandBase> _commands;
+
         public BaseViewModel()
         {
+            CommandManager.RequerySuggested += CommandManager_RequerySuggested;
             Loaded();
         }
 
         public abstract void Loaded();
+
+        private void CommandManager_RequerySuggested(object sender, EventArgs e)
+        {
+            RaiseCanExecuteChangedOnCommands();
+        }
+
+        protected void RaiseCanExecuteChangedOnCommands()
+        {
+            if (_commands == null)
+            {
+                var delegateCommands = GetDelegateCommands(this.GetType(), this);
+                _commands = delegateCommands.Where(cm => cm != null).ToList();
+            }
+            if (_commands != null)
+                _commands.ForEach(cm => cm.RaiseCanExecuteChanged());
+
+            IEnumerable<DelegateCommandBase> GetDelegateCommands(Type type, object instance)
+            {
+                return type.GetProperties().Where(p => typeof(DelegateCommandBase).IsAssignableFrom(p.PropertyType)).Select(p => p.GetValue(instance) as DelegateCommandBase);
+            }
+        }
     }
 }
